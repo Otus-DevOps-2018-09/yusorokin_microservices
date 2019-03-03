5a50e9a6979d2cda36dde84903807a7579394fcf
Yuriy Sorokin <yurich00@gmail.com>
2019-03-03 17:14:15 +0600

    HW22 Основные модели безопасности и контроллеры в Kubernetes. Основное задание и задание со *

---
+++ kubernetes/terraform/main.tf
@@ -0,0 +1,69 @@
+provider "google" {
+  version = "1.4.0"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
+
+resource "google_container_cluster" "primary" {
+  name = "reddit-cluster-terraform"
+  zone = "us-central1-a"
+  initial_node_count = 2
+
+  enable_legacy_abac = "false"
+
+  addons_config {
+    kubernetes_dashboard {
+      disabled = "false"
+    }
+  }
+
+  master_auth {
+    username = ""
+    password = ""
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    disk_size_gb = 20
+    machine_type = "g1-small"
+
+    tags = ["https-server"]
+  }
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
+
+resource "google_compute_firewall" "k8s_firewall" {
+  name    = "allow-k8s-ui"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["30000-32767"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+# The following outputs allow authentication and connectivity to the GKE Cluster
+# by using certificate-based authentication.
+output "client_certificate" {
+  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
+}
+
+output "client_key" {
+  value = "${google_container_cluster.primary.master_auth.0.client_key}"
+}
+
+output "cluster_ca_certificate" {
+  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
+}
